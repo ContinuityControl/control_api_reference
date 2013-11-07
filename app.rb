c8f3b0df0a58ab3995178921a1066e739972d8fa
Benjamin Oakes <boakes@continuity.net>
2013-11-07 12:45:29 -0500

    First iteration of documentation

--- app.rb
+++ app.rb
@@ -21,6 +21,10 @@ end
 #
 # Check the API status.  Useful for testing authentication without knowing other information.
 #
+# Example request:
+#
+#     GET /v1/status
+#
 # Example response:
 #
 #     {"description":"up"}
@@ -47,8 +51,8 @@ end
 #
 # Request fields:
 #
-#   * `template_to_do_uuid`: The UUID for the TemplateToDo that will be distributed.  This can be found in settings.
-#   * `assignee_emails`: A comma separated list of emails of Users that will receive the DistributedToDos.
+#   * `template_to_do_uuid`: **Required**.  The UUID for the TemplateToDo that will be distributed.  This can be found in settings.
+#   * `assignee_emails`: **Required**.  A comma separated list of emails of Users that will receive the DistributedToDos.
 #   * `content`: JSON text of values to pre-fill in the DistributedToDo.  Field names are available under "Settings".
 #
 # Example response:
@@ -67,28 +71,65 @@ post '/distributed_to_dos' do
   # TODO: receive POST from form and distribute as requested
 end
 
-# ## GET /v1/distributed_to_dos
-#
-# TODO
-#
-# Example response:
-#
-#     TODO
-#
-# Response fields:
-#
-#   * TODO
-#
-
 # ## /v1/distributed_to_dos/:id
 #
-# TODO
+# Get the current state of a distributed_to_do with the ID `:id`
+#
+# Example request:
+#
+#     GET /v1/distributed_to_dos/4567
 #
 # Example response:
 #
-#     TODO
+#     {
+#       "id": "4567",
+#       "transaction_id": "f81d4fae-7dec-11d0-a765-00a0c91e6bf6",
+#       "created_at": "2013-11-02T12:34:46Z",
+#       "completed_at": "2013-11-07T12:34:56Z",
+#       "assignments": [
+#         {
+#           "user_email": "bobama@example.com",
+#           "finished_on": "2013-11-07"
+#         },
+#         {
+#           "user_email": "bclinton@example.com",
+#           "finished_on": null
+#         }
+#       ]
+#     }
 #
 # Response fields:
 #
-#   * TODO
+#   * `id`: Unique ID for this DistributedToDo. Not guaranteed to be numeric.
+#   * `transaction_id`: Transaction ID of the request that created this DistributedToDo.  Only present for DistributedToDos created via the API.
+#   * `created_at`: ISO8601 datetime of the creation of this DistributedToDo, in UTC.
+#   * `completed_at`: ISO8601 date of when the DistributedToDo was completed, in UTC.
+#   * `assignments`: Array
+#     * `user_email`: User email of DistributedToDo assignment
+#     * `finished_on`: ISO8601 date on which the assignment was completed
 #
+get '/distributed_to_dos/:id' do
+  # TODO: render a page with the information in the response
+end
+
+# ## GET /v1/distributed_to_dos
+#
+# Example request
+#
+#     GET /v1/distributed_to_dos
+#
+# Example response:
+#
+#     {
+#       "distributed_to_dos": [
+#         // See above
+#       ]
+#     }
+#
+# Response fields:
+#
+#   * `distributed_to_dos`: an Array of DistributedToDos
+#
+get '/distributed_to_dos' do
+  # TODO: render a page with links to each distributed_to_do
+end
