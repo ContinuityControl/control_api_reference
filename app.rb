74aed5ac3929fd5340077aa7a89ec23d5a818e10
Benjamin Oakes and Dan Bernier <dev+boakes+dbernier@continuity.net>
2013-11-19 17:02:10 -0500

    Nest POST fields within distributed_to_do[]

--- app.rb
+++ app.rb
@@ -67,15 +67,15 @@ end
 # ### Example request
 #
 #     POST /v1/distributed_to_dos
-#     template_to_do_api_id=12345678-1234-5678-1234-567812345678
-#     assignee_emails=["bobama@example.com","gwbush@example.com","bclinton@example.com","gbush@example.com","rreagan@example.com","jcarter@example.com","gford@example.com","rnixon@example.com"]
-#     content={"field1":"value1"}
+#     distributed_to_do[template_to_do_api_id]=12345678-1234-5678-1234-567812345678
+#     distributed_to_do[assignee_emails]=["bobama@example.com","gwbush@example.com","bclinton@example.com","gbush@example.com","rreagan@example.com","jcarter@example.com","gford@example.com","rnixon@example.com"]
+#     distributed_to_do[content]={"field1":"value1"}
 #
 # ### Request fields
 #
-#   * `template_to_do_api_id`: **Required**.  The UUID for the TemplateToDo that will be distributed.  This can be found in Continuity Control under "Settings".
-#   * `assignee_emails`: **Required**.  A JSON Array of email addresses of Users that will receive the DistributedToDos.
-#   * `content`: JSON text of values to pre-fill in the DistributedToDo.  Field names are available in Continuity Control under "Settings".
+#   * `distributed_to_do[template_to_do_api_id]`: **Required**.  The UUID for the TemplateToDo that will be distributed.  This can be found in Continuity Control under "Settings".
+#   * `distributed_to_do[assignee_emails]`: **Required**.  A JSON Array of email addresses of Users that will receive the DistributedToDos.
+#   * `distributed_to_do[content]`: JSON text of values to pre-fill in the DistributedToDo.  Field names are available in Continuity Control under "Settings".
 #
 # ### Example responses
 #
